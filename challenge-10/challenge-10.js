10f9c8cfc92854561bb4451a39067df0d6b41e5d
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-03-24 15:01:36 -0300

    Update challenge-10.js

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -13,7 +13,7 @@ resolver o problema corretamente.
 var five = Number('5'); // Ou eu mudava o '5' para 5
 console.log( five + ' é número?', typeof five === 'number' );
 
-var concat = String('10 + 10'); // Ou eu mudava o 10 + 10 para '10 + 10' 
+var concat = String(10) + 10; // Ou eu mudava o 10 + 10 para '10 + 10' 
 console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
 /*
